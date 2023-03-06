245e464c920b3b4b8ecb92a7850d74356a0a2c2e
stellawessels <stella@wessels.nl>
2023-03-06 11:04:44 +0100

    C updated

--- assignment2/data_analysis.py
+++ assignment2/data_analysis.py
@@ -7,6 +7,7 @@ data_list = ('IAGOS_timeseries_2019050116041591.txt', 'IAGOS_timeseries_20190430
              'IAGOS_timeseries_2019021102051591.txt', 'IAGOS_timeseries_2019021011295591.txt')
 file_index = 7
 
+# Calculation saturation pressure
 T = 250
 a = [-6.0969385e3, 2.12409642e1, -2.711193e-2, 1.673952e-5, 2.433502]
 es = np.exp(a[0] * T**(-1) + a[1] + a[2] * T + a[3] * T**2 + a[4] * np.log(T))
@@ -14,7 +15,7 @@ print(es)
 
 data = pd.read_csv(data_list[file_index], sep=' ', skiprows=70)
 timestep = 10 # seconds
-LTO_limit = 914.4 # meters
+LTO_limit = 914.4 # meters (based on 3000 ft ICAO)
 
 ## Data Analysis
 data = data.query('baro_alt_AC_val < 7') # remove all lines with measuring errors
@@ -23,7 +24,7 @@ data = data.loc[:,('baro_alt_AC','air_press_AC','air_temp_AC','H2O_gas_PC2','gro
 data = data.assign(distance = data['ground_speed_AC'] * timestep) # append column with distance per timestep
 data = data.assign(saturation_pressure = np.exp(a[0] * data['air_temp_AC']**(-1) + a[1] + a[2] * data['air_temp_AC']
                                                 + a[3] * data['air_temp_AC']**2 + a[4] * np.log(data['air_temp_AC'])))
-data = data.assign(vapour_pressure = data['air_press_AC'] * data['H2O_gas_PC2'] * 1e-6)
+data = data.assign(vapour_pressure = data['air_press_AC'] * data['H2O_gas_PC2'] / 1e-6)
 data_nonLTO = data.query('baro_alt_AC > 914.4')
 data_temp = data.query('air_temp_AC < 235')
 data_ISSR = data.query('vapour_pressure > saturation_pressure')
@@ -33,9 +34,17 @@ print(data_ISSR.shape[0])
 full_distance = data['distance'].sum() / 1000 # distance in km
 distance = data_nonLTO['distance'].sum() / 1000
 
+print('Distance total in km', full_distance)
+print('Distance total in km non LTO', distance)
+
+#C Temperature treshold
 distance_temp = data_temp['distance'].sum() / 1000
 
 temp_percentage = distance_temp / distance
 print(temp_percentage)
+
+#D Schmidt-Appleman Criterion
+
+
 
 # print(data.describe(include = 'all'))
