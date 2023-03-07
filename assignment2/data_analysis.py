0fa81b477639d0fdefbf7284c762822103fe2ce8
WouterOffringa <wouter.offringa@outlook.com>
2023-03-07 16:53:14 +0100

    update of SAC

--- assignment2/data_analysis.py
+++ assignment2/data_analysis.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 data_list = ('IAGOS_timeseries_2019050116041591.txt', 'IAGOS_timeseries_2019043020424591.txt',
              'IAGOS_timeseries_2019043004153591.txt', 'IAGOS_timeseries_2019042914412591.txt',
              'IAGOS_timeseries_2019021216295591.txt', 'IAGOS_timeseries_2019021122212591.txt',
              'IAGOS_timeseries_2019021102051591.txt', 'IAGOS_timeseries_2019021011295591.txt')
-file_index = 3
+file_index = 0
 
 # Calculation saturation pressure
-T = 250
 a = [-6.0245282e3, 2.932707e1, 1.0613868e-2, -1.3198825e-5, -4.9382577e-1]
-es = np.exp(a[0] * T**(-1) + a[1] + a[2] * T + a[3] * T**2 + a[4] * np.log(T))
 
 # Parameters
 cp = 1.0035e3 #J/kg/K
@@ -40,7 +39,8 @@ data_ISSR = data.query('vapour_pressure > saturation_pressure')
 
 # Calculation liquid 'over water' saturation pressure
 a_liq = [-6.0969385e3, 2.12409642e1, -2.711193e-2, 1.673952e-5, 2.433502]
-data = data.assign(e_liq = np.exp(a_liq[0] * data['air_temp_AC']**(-1) + a_liq[1] + a_liq[2] * data['air_temp_AC'] + a_liq[3] * data['air_temp_AC']**2 + a_liq[4] * np.log(data['air_temp_AC'])))
+data = data.assign(e_liq = np.exp(a_liq[0] * data['air_temp_AC']**(-1) + a_liq[1] + a_liq[2] * data['air_temp_AC'] +
+                                  a_liq[3] * data['air_temp_AC']**2 + a_liq[4] * np.log(data['air_temp_AC'])))
 
 
 ## Calculations & Results
@@ -49,7 +49,7 @@ data = data.assign(e_liq = np.exp(a_liq[0] * data['air_temp_AC']**(-1) + a_liq[1
 full_distance = data['distance'].sum() / 1000 # distance in km
 distance = data_nonLTO['distance'].sum() / 1000
 
-print('Distance total in km', full_distance)
+print('A) Distance total in km', full_distance)
 print('Distance total in km non LTO', distance)
 
 #B ISSR
@@ -66,22 +66,30 @@ print('C) Temperature Threshold = ', temp_percentage, '%')
 #D Schmidt-Appleman Criterion aircraft 1
 
 data = data.assign(G_1= data['air_press_AC'] * cp * (M_air/M_H20) * (EI_1/((1-n_1)*Q_1)))
-data = data.assign(T_LM_1 = -46.46 + 9.43* np.log(data['G_1']-0.053) + 0.720 * (np.log(data['G_1'] - 0.053))**2)
+data = data.assign(T_LM_1 = -46.46 + 9.43* np.log(data['G_1']-0.053) + 0.720 * (np.log(data['G_1'] - 0.053))**2 + 273)
+data = data.assign(e_liq1 = np.exp(a_liq[0] * data['T_LM_1']**(-1) + a_liq[1] + a_liq[2] * data['T_LM_1'] +
+                                  a_liq[3] * data['T_LM_1']**2 + a_liq[4] * np.log(data['T_LM_1'])))
 data = data.assign(LeftSAC_1= data['vapour_pressure'] + data['G_1'] * (data['T_LM_1']-data['air_temp_AC']))
-data = data.assign(RightSAC_1 = data['e_liq']*data['T_LM_1'])
-data_SAC_1 = data.query('LeftSAC_1 > RightSAC_1')
+data_SAC_1 = data.query('LeftSAC_1 > e_liq1')
 
 data = data.assign(G_2= data['air_press_AC'] * cp * (M_air/M_H20) * (EI_2/((1-n_2)*Q_2)))
-data = data.assign(T_LM_2 = -46.46 + 9.43* np.log(data['G_2']-0.053) + 0.720 * (np.log(data['G_2'] - 0.053))**2)
+data = data.assign(T_LM_2 = -46.46 + 9.43* np.log(data['G_2']-0.053) + 0.720 * (np.log(data['G_2'] - 0.053))**2 + 273)
+data = data.assign(e_liq2 = np.exp(a_liq[0] * data['T_LM_2']**(-1) + a_liq[1] + a_liq[2] * data['T_LM_2'] +
+                                  a_liq[3] * data['T_LM_2']**2 + a_liq[4] * np.log(data['T_LM_2'])))
 data = data.assign(LeftSAC_2= data['vapour_pressure'] + data['G_2'] * (data['T_LM_2']-data['air_temp_AC']))
-data = data.assign(RightSAC_2 = data['e_liq']*data['T_LM_2'])
-data_SAC_2 = data.query('LeftSAC_2 > RightSAC_2')
+data_SAC_2 = data.query('LeftSAC_2 > e_liq2')
 
-distance_SAC_1 = data_SAC_1['distance'].sum()/1000
+
+data= data.query('air_temp_AC < 235')
+plt.plot(data["air_temp_AC"], data["saturation_pressure"])
+plt.plot(data["air_temp_AC"], data["vapour_pressure"])
+plt.show()
+
+distance_SAC_1 = data_SAC_1['distance'].sum() / 1000
 distance_SAC_1_percentage = distance_SAC_1 / distance * 100
 print('D) Distance SAC for 1 = ', distance_SAC_1_percentage, '%')
 
-distance_SAC_2 = data_SAC_2['distance'].sum()/1000
+distance_SAC_2 = data_SAC_2['distance'].sum() / 1000
 distance_SAC_2_percentage = distance_SAC_2 / distance * 100
 print('Distance SAC for 2 = ', distance_SAC_2_percentage, '%')
 
